6fa379a5d33c925a1c4c8f5af44c79ac821970bf
fracabu <100199965+fracabu@users.noreply.github.com>
2022-09-14 13:02:08 +0200

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Braintree\Gateway;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(Gateway::class, function($app){
+            return new Gateway([
+                
+                'environment' => 'sandbox',
+                'merchantId' => '4sjx493rm6vt8q2c',
+                'publicKey' => 'vxcgrmnyyzb54s38',
+                'privateKey' => 'e349ea0792ea4553bbb724c4fd7a924e'
+            ]);
+        });
     }
 }
